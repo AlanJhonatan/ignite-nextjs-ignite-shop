7d435b61d9bf89b58936060ecfb35ee0a7a19af8
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-25 22:46:48 -0300

    feat: add App styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 
+
+import logoImage from '../assets/logo-ignite-shop.svg'
+import { Container, Header } from "../styles/pages/app"
+
 globalStyles()
 
 export default function MyApp({ Component, pageProps }: AppProps) {  
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImage.src} alt="" />
+      </Header>
+      
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
