4f8c27c35004043aca15b7440cfea383209742f5
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-26 21:11:47 -0300

    feat: add Home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,29 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import shirt1 from '../assets/1.png'
+import shirt2 from '../assets/2.png'
+import shirt3 from '../assets/3.png'
 
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt=""  />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt=""  />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
