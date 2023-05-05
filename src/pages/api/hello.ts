fb645faab61379d82aba43cf2b82cf1810c5ac31
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-05-04 23:58:51 -0300

    feat: add function to handle product price id

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { stripe } from "../../lib/stripe";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.json({ message: 'Hello World' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const priceId = ''
+
+  const successUrl = `${process.env.NEXT_URL}/success`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+  
+  const checkoutSession = await stripe.checkout.sessions.create({
+    mode: 'payment',
+    line_items: [
+      { price: priceId, quantity: 1, }
+    ],
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+  })
 }
