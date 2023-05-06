968b8b28b275992d357af77f435730421e4d32e8
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-05-05 22:43:32 -0300

    feat: add checkout state in buy button

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { GetStaticPaths, GetStaticProps } from "next"
 import Image from "next/image"
+import { useState } from "react"
 import Stripe from "stripe"
 import { stripe } from "../../lib/stripe"
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/products"
@@ -17,9 +18,11 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+  
   async function handleBuyProduct() {
     try {
-      console.log({product})
+      setIsCreatingCheckoutSession(true)
       const response = await axios.post('/api/checkout', {
         priceId: product.defaultPriceId
       })
@@ -28,6 +31,7 @@ export default function Product({ product }: ProductProps) {
 
       window.location.href = checkoutUrl
     } catch (err) {
+      setIsCreatingCheckoutSession(false)
       alert('Falha ao redirecionar ao checkout.')
     }
   }
@@ -44,7 +48,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button onClick={handleBuyProduct}>Comprar</button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>Comprar</button>
       </ProductDetails>
     </ProductContainer>
   )
